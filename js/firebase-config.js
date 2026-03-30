eefb13f05804fbbfdf51cce62444165fd9e228ed
Dinkar Khergade <dinkar.khergade1995@live.com>
2026-03-31 00:10:33 +0530

    Refactor Firebase initialization with error handling
    Replaced Firebase SDK initialization with a try-catch block for error handling.
    p1.6

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWV2sgzBcJG3lFNFLdTebHR5K9D12o29g",
-  authDomain: "vidyaclasses-6de90.firebaseapp.com",
-  projectId: "vidyaclasses-6de90",
-  storageBucket: "vidyaclasses-6de90.firebasestorage.app",
-  messagingSenderId: "116164197592",
-  appId: "1:116164197592:web:1dd31b589daa5fe7b2ebad",
-  measurementId: "G-JTPGG5D32G"
+    apiKey: "AIzaSyBWV2sgzBcJG3lFNFLdTebHR5K9D12o29g",
+    authDomain: "vidyaclasses-6de90.firebaseapp.com",
+    projectId: "vidyaclasses-6de90",
+    storageBucket: "vidyaclasses-6de90.firebasestorage.app",
+    messagingSenderId: "116164197592",
+    appId: "1:116164197592:web:1dd31b589daa5fe7b2ebad",
+    measurementId: "G-JTPGG5D32G"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+try {
+    firebase.initializeApp(firebaseConfig);
+    window.auth = firebase.auth();
+    window.db = firebase.firestore();
+} catch (error) {
+    console.error("Firebase Initialization Error:", error);
+}
